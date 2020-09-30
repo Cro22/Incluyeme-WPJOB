2d74174ce51b13b8fe9f0a2272d06cc3ab228fc5
Jesus <jesus.nunez2050@gmail.com>
2020-09-30 05:18:15 -0400

    Ver: 1.7.6 Change query;

--- include/lib/WP_Incluyeme.php
+++ include/lib/WP_Incluyeme.php
@@ -89,8 +89,29 @@ class WP_Incluyeme extends WP_Filters_Incluyeme
         } else {
             $query .= " WHERE meta.name = 'tipo_discapacidad' and " . $prefix . "wpjb_company.user_id = " . self::getUserId();
         }
+    
+        if (self::getEstudiosCheck() === 1 && self::getEstudiosCheckF() !== 1) {
+            $qry2 =self::getJob() !== null ?  " AND " . $prefix . "wpjb_job.id = " . self::getJob() . ' ' : '';
+            $query .= " AND " . $prefix . "users.ID NOT IN (SELECT
+    " . $prefix . "users.ID AS users_id
+  FROM " . $prefix . "wpjb_resume
+    INNER JOIN " . $prefix . "users
+      ON " . $prefix . "users.ID = " . $prefix . "wpjb_resume.user_id
+    LEFT OUTER JOIN " . $prefix . "wpjb_resume_detail edu
+      ON " . $prefix . "wpjb_resume.id = edu.resume_id
+      AND 2 = edu.type
+      INNER JOIN wp_wpjb_application
+    ON wp_wpjb_resume.user_id = wp_wpjb_application.user_id
+     INNER JOIN wp_wpjb_job
+    ON wp_wpjb_application.job_id = wp_wpjb_job.id
+     LEFT OUTER JOIN wp_wpjb_company
+    ON wp_wpjb_job.employer_id = wp_wpjb_company.id
+  WHERE   edu.is_current = 1 AND " . $prefix . "wpjb_company.user_id = " . self::getUserId() . $qry2."
+    GROUP BY " . $prefix . "users.ID)";
+        }
         $group = "
   GROUP BY   " . $prefix . "users.user_email";
+     
         if ($this->getSearchPhrase() !== null) {
             $queries = $this->addQueries($query, true);
         } else {
@@ -115,16 +136,6 @@ class WP_Incluyeme extends WP_Filters_Incluyeme
                     }
                 }
                 $response = array_values($response);
-                
-                if (self::getEstudiosCheck() === 1 && self::getEstudiosCheckF() !== 1) {
-                    $response = array_unique($response, SORT_REGULAR);
-                    foreach ($response as $key => $search) {
-                        $rating = $this->searchComplete($response[$key]->users_id);
-                        if ($rating === false) {
-                            unset($response[$key]);
-                        }
-                    }
-                }
                 return array_unique($response, SORT_REGULAR);
             }
             return $response = [];
@@ -192,73 +203,4 @@ WHERE ' . $prefix . 'wpjb_meta.name = "rating"
         }
     }
     
-    private function searchComplete($userID)
-    {
-        global $wpdb;
-        $prefix = $wpdb->prefix;
-        $completes = "SELECT
-  " . $prefix . "users.ID AS users_id
-FROM " . $prefix . "wpjb_resume
-  INNER JOIN " . $prefix . "users
-    ON " . $prefix . "users.ID = " . $prefix . "wpjb_resume.user_id
-  LEFT OUTER JOIN " . $prefix . "wpjb_resume_search
-    ON " . $prefix . "wpjb_resume.id = " . $prefix . "wpjb_resume_search.resume_id
-  LEFT OUTER JOIN " . $prefix . "wpjb_application
-    ON " . $prefix . "wpjb_resume.user_id = " . $prefix . "wpjb_application.user_id
-  LEFT OUTER JOIN " . $prefix . "wpjb_job
-    ON " . $prefix . "wpjb_application.job_id = " . $prefix . "wpjb_job.id
-  LEFT OUTER JOIN " . $prefix . "wpjb_meta_value
-    ON " . $prefix . "wpjb_application.id = " . $prefix . "wpjb_meta_value.object_id
-  LEFT OUTER JOIN " . $prefix . "wpjb_meta
-    ON " . $prefix . "wpjb_meta_value.meta_id = " . $prefix . "wpjb_meta.id
-  LEFT OUTER JOIN " . $prefix . "wpjb_tagged
-    ON " . $prefix . "wpjb_resume.id = " . $prefix . "wpjb_tagged.id
-  LEFT OUTER JOIN " . $prefix . "posts
-    ON " . $prefix . "wpjb_resume.post_id = " . $prefix . "posts.ID
-  LEFT OUTER JOIN " . $prefix . "usermeta
-    ON " . $prefix . "users.ID = " . $prefix . "usermeta.user_id
-  LEFT OUTER JOIN " . $prefix . "wpjb_company
-    ON " . $prefix . "wpjb_job.employer_id = " . $prefix . "wpjb_company.id
-  LEFT OUTER JOIN " . $prefix . "wpjb_resume_detail edu
-    ON " . $prefix . "wpjb_resume.id = edu.resume_id
-    AND 2 = edu.type
-WHERE " . $prefix . "wpjb_company.user_id = " . self::getUserId() . "
-AND " . $prefix . "users.ID = " . $userID . "
-AND " . $prefix . "users.ID NOT IN (SELECT
-    " . $prefix . "users.ID AS users_id
-  FROM " . $prefix . "wpjb_resume
-    INNER JOIN " . $prefix . "users
-      ON " . $prefix . "users.ID = " . $prefix . "wpjb_resume.user_id
-    LEFT OUTER JOIN " . $prefix . "wpjb_resume_detail edu
-      ON " . $prefix . "wpjb_resume.id = edu.resume_id
-      AND 2 = edu.type
-  WHERE  " . $prefix . "users.ID = " . $userID . " AND edu.is_current = 1
-  GROUP BY " . $prefix . "users.ID)
-GROUP BY " . $prefix . "users.user_email,
-         " . $prefix . "users.display_name,
-         " . $prefix . "wpjb_resume.phone,
-         " . $prefix . "wpjb_resume.description,
-         " . $prefix . "wpjb_application.applicant_name,
-         " . $prefix . "wpjb_job.job_title,
-         " . $prefix . "posts.guid,
-         " . $prefix . "wpjb_meta.name,
-         " . $prefix . "wpjb_meta_value.value,
-         " . $prefix . "usermeta.meta_key,
-         " . $prefix . "usermeta.meta_value,
-         " . $prefix . "wpjb_resume.candidate_state,
-         " . $prefix . "wpjb_resume.candidate_location,
-         " . $prefix . "users.ID
-LIMIT 1";
-        try {
-            
-            $result = $this->executeQueries($completes);
-            if (count($result) === 0) {
-                return false;
-            }
-            return true;
-        } catch (Exception $e) {
-            throw new Exception('Invalid data passing to this function: getExtraData' . $e);
-        }
-        
-    }
 }
